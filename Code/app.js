d60c98411b949366cc11d95954bff27e0d8f0c20
Jalla-js <73756741+Jalla-js@users.noreply.github.com>
2026-04-14 22:23:17 +0100

    Refactor map initialization and site loading logic

--- Code/app.js
+++ Code/app.js
@@ -1,12 +1,8 @@
 const map = L.map('map').setView([53.1, -4.1], 10);
 
-var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
-	minZoom: 0,
-	maxZoom: 20,
-	attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-	ext: 'png'
-});
-Stadia_AlidadeSmoothDark.addTo(map);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19
+}).addTo(map);
 
 let markers = L.layerGroup().addTo(map);
 
